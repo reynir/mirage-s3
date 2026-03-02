c16defe16522ed4582da31071eaff38f241adbd8
Reynir Björnsson <reynir@reynir.dk>
2026-03-02 08:55:53 +0100

    remove pins - I removed PPX's in aws-s3

--- config.ml
+++ config.ml
@@ -10,15 +10,6 @@ let mirage_s3 =
       ~pin:"git+https://github.com/reynir/aws-s3.git#mirage";
     package "aws-s3"
       ~pin:"git+https://github.com/reynir/aws-s3.git#mirage";
-    package "ppx_protocol_conv"
-      ~pin:"git+https://github.com/reynir/ppx_protocol_conv.git#mirage";
-    package "ppx_protocol_conv_xmlm"
-      ~pin:"git+https://github.com/reynir/ppx_protocol_conv.git#mirage";
-    package "ppx_protocol_conv_json"
-      ~pin:"git+https://github.com/reynir/ppx_protocol_conv.git#mirage";
-    (* opam-monorepo is hot garbage, and this is an attempt at directing the solver.
-       "User requested = 5.2.1" my ass!!  *)
-    package "ocaml" ~max:"5.0.0";
   ] in
   main ~packages ~deps:[ dep dns ] ~pos:__POS__ "Unikernel.Make" (stackv4v6 @-> happy_eyeballs @-> job)
 
